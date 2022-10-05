c10e89d53c14c38e5f633f4ca533d1d416b65a3a
ghghtygh <ghghtygh@naver.com>
2022-10-06 01:43:50 +0900

    add: 테스트 후 삭제예정

---
+++ src/main/java/com/bside/someday/config/web/WebConfig.java
@@ -0,0 +1,16 @@
+package com.bside.someday.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	// FIXME. 삭제예정
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost");
+	}
+}
